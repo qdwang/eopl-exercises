2ca8da404f35c2c00eb390f6e3caa4023268b236
qdwang <wangqidong@gmail.com>
2015-11-03 16:48:52 +0800

    add funcs

--- chapters/IndectiveSetsOfData.rkt
+++ chapters/IndectiveSetsOfData.rkt
@@ -23,3 +23,48 @@
           (cdr lst)
           (cons (car lst) (remove-first item (cdr lst))))))
 
+(: occurs-free? (-> Symbol (Rec Expr (Listof (U Symbol Expr))) Boolean))
+(define (occurs-free? symbol expr)
+ (if (null? expr)
+     #f
+     (if (symbol? (car expr))
+         (if (equal? symbol (car expr))
+             #t
+             (occurs-free? symbol (cdr expr)))
+         (or (occurs-free? symbol (car expr))
+             (occurs-free? symbol (cdr expr))))))
+
+
+(: subst (-> Symbol Symbol (Rec Expr (Listof (U Symbol Expr))) (Rec Expr (Listof (U Symbol Expr)))))
+(define (subst new old expr)
+  (if (null? expr)
+      expr
+      (if (list? (car expr))
+          (cons (subst new old (car expr)) (subst new old (cdr expr)))
+          (if (equal? old (car expr))
+              (cons new (subst new old (cdr expr)))
+              (cons (car expr) (subst new old (cdr expr)))))))
+
+
+(: number-elements-from (All (A) (-> (Listof A) Integer (Listof (List Integer A)))))
+(define (number-elements-from lst n)
+  (if (null? lst)
+      lst
+      (cons (list n (car lst)) (number-elements-from (cdr lst) (+ n 1)))))
+
+(: number-elements (All (A) (-> (Listof A) (Listof (List Integer A)))))
+(define (number-elements lst)
+  (number-elements-from lst 0))
+
+(: list-sum (-> (Listof Integer) Integer))
+(define (list-sum lst)
+  (if (null? lst)
+      0
+      (+ (car lst) (list-sum (cdr lst)))))
+
+(: partial-vector-sum (-> (Vectorof Integer) Integer Integer))
+(define (partial-vector-sum vec n)
+  (if (or (null? vec) (zero? n))
+      (vector-ref vec n)
+      (+ (vector-ref vec n) (partial-vector-sum vec (- n 1)))))
+
