dafa8584a02709b8ad1f8ddbfb188a90b68d7749
qdwang <wangqidong@gmail.com>
2015-11-05 17:03:24 +0800

    add rec struct

--- chapters/DataAbstraction.rkt
+++ chapters/DataAbstraction.rkt
@@ -16,3 +16,10 @@
       (if (equal? (car env) symbol)
           (cadr env)
           (apply-env (cast (list-ref env 2) Env) symbol))))
+
+
+(struct var-exp ([var : Symbol]))
+(struct lambda-exp ([bound-var : Symbol] [body : (U Null lc-exp)]))
+(struct app-exp ([rator : (U Null lc-exp)] [rand : (U Null lc-exp)]))
+(struct lc-exp ([var-exp : var-exp] [lambda-exp : lambda-exp] [app-exp : app-exp]))
+  
