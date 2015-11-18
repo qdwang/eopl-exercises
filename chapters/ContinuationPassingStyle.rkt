d4c1d2a7d1cf672519a5d4ebfa95f8631c8a1bab
qdwang <wangqidong@gmail.com>
2015-11-18 17:39:11 +0800

    undone

--- chapters/ContinuationPassingStyle.rkt
+++ chapters/ContinuationPassingStyle.rkt
@@ -1,20 +1,38 @@
 #lang typed/racket
 
+(define-type Exp (Rec exp (U Symbol Integer (Listof exp))))
+
+
+
+(: cps-convert (-> Exp Exp))
+(define (cps-convert input-exp)
+
+  (define [v-num : Integer] 0)
+    
+  (: cps-s (-> Exp Exp))
+  (define (cps-s input-exp)
+    (match input-exp
+      [x (quasiquote (cont (unquote input-exp)))]
+      
+      [('lambda args body)
+       (quasiquote (lambda (unquote (reverse (cons 'cont args))) (unquote (cps body))))]
+      
+      [('if cond-exp then-exp else-exp)
+       (quasiquote (if (unquote cond-exp) 
+                          
+
+
+
 ; for test
-(: f (-> Integer Integer))
-(define (f x) 1)
-(: g (-> Integer Integer Integer))
-(define (g x z) 1)
-(define y 2)
-(: h (-> Integer Integer Integer Integer))
-(define (h a b c) 1)
-(define (test-exp)
-  (lambda ([x : Integer])
-    (cond
-      ((zero? x) 17)
-      ((= x 1) (f (- x 13)))
-      ((= x 2) (+ 22 (- x 3) x))
-      ((= x 3) (+ 22 (f x) 37))
-      ((= x 4) (g 22 (f x)))
-      ((= x 5) (+ 22 (f x) 33 (g y 2)))
-      (else (h (f x) (- 44 y) (g y 3))))))
+(define test-exp1
+'(lambda (x)
+  (if (zero? x)
+      (+ a (f b) 5 6)
+      (f c))))
+
+;to
+;empty cont will be (lambda (x) x)
+'(lambda (x cont)
+  (if (zero? x)
+      ((f b) (lambda (v1) (cont (+ a v1 5 6)))) 
+      (f c cont)))
